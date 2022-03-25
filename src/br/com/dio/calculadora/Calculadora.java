4f394f05c30132f9168ebadcc1786f4bef370472
CristianoDamasceno <cristiano.damasceno33@gmail.com>
2022-03-25 18:28:08 -0300

    First Commit

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtracao((a, b);
+        int multiplicar = multiplicacao((a, b);
+        int dividir = divisao((a, b);
+
+        System.out.prinln("Soma: " + somar);
+        System.out.println("Subtracao: " + subtrair);
+        System.out.println("Multiplicacao: " + multiplicar);
+        System.out.println("Divisa: " + dividir);
+
+        );
+    }
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+    public static int subtracao(int a, int b){
+        return a - b;
+    }
+    public static int multiplicacao(int a, int b){
+        return a * b;
+    }
+
+    public static int divisao(int a, int b){
+        return a / b;
+    }
+}
